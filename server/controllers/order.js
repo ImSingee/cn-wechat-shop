3d15ff8d561195ea539d38f4a8042b6f79d080ef
Singee <imsingee@gmail.com>
2018-09-15 12:25:36 +0800

    change export default to module.exports

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -1,28 +1,28 @@
-const DB = require('../utils/db.js')
+const DB = require('../utils/db.js');
 
-export default {
-  add: async ctx => {
-    let user = ctx.state.$wxInfo.userinfo.openId
+module.exports = {
+    add: async ctx => {
+        let user = ctx.state.$wxInfo.userinfo.openId
 
-    // 获取订单产品列表
-    let productList = ctx.request.body.list || []
+        // 获取订单产品列表
+        let productList = ctx.request.body.list || []
 
-    // 插入订单至 order_user 表
-    let order = await DB.query('INSERT INTO order_user(user) VALUES (?)', [user])
+        // 插入订单至 order_user 表
+        let order = await DB.query('INSERT INTO order_user(user) VALUES (?)', [user])
 
-    let orderId = order.insertId
+        let orderId = order.insertId
 
-    let param = []
-    let query = []
+        let param = []
+        let query = []
 
-    productList.forEach(product => {
-      query.push('(?,?,?)')
+        productList.forEach(product => {
+            query.push('(?,?,?)')
 
-      param.push(orderId)
-      param.push(product.id)
-      parampush(product.count || 1)
-    })
+            param.push(orderId)
+            param.push(product.id)
+            parampush(product.count || 1)
+        })
 
-    await DB.query('INSERT INTO order_product(order_id, product_id, count) VALUES ' + query.join(', '), param)
-  }
+        await DB.query('INSERT INTO order_product(order_id, product_id, count) VALUES ' + query.join(', '), param)
+    }
 }
