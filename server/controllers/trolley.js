110c8edc80dbe0a17eafbd346ec037d36371b089
Singee <imsingee@gmail.com>
2018-09-16 00:31:10 +0800

    server add to trolley

---
+++ server/controllers/trolley.js
@@ -0,0 +1,17 @@
+const DB = require('../utils/db.js')
+
+module.exports = {
+    add: async ctx => {
+        let user = ctx.state.$wxInfo.userinfo.openId
+        let product_id = ctx.request.body.product.id
+
+        let obj = await DB.query('SELECT * FROM trolley_user WHERE trolley_user.id = ? AND trolley_user.user = ?', [product_id, user])
+
+        if (obj.length > 0) {
+            let count = obj[0].count
+            DB.query('UPDATE trolley_user SET count = ? WHERE trolley_user.id = ? AND trolley_user.user = ?', [count, product_id, user])
+        } else {
+            DB.query('INSERT INTO trolley_user(id, count, user) VALUES (?, ?, ?)', [product_id, 1, user])
+        }
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ const controllers = require('../controllers')
 
 // 从 sdk 中取出中间件
 // 这里展示如何使用 Koa 中间件完成登录态的颁发与验证
-const { auth: { authorizationMiddleware, validationMiddleware } } = require('../qcloud')
+const {auth: {authorizationMiddleware, validationMiddleware}} = require('../qcloud')
 
 // --- 登录与授权 Demo --- //
 // 登录接口
@@ -41,5 +41,8 @@ router.get('/product/:id', controllers.product.detail)
 router.post('/order', validationMiddleware, controllers.order.add)
 // GET 获取订单
 router.get('/order', validationMiddleware, controllers.order.list)
+
+// PUT 添加到购物车
+router.put('/trolley', validationMiddleware, controllers.trolley.add)
 
 module.exports = router
