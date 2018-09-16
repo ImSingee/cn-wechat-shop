0dc05263499ef5d6bccffc84d697d1281142e85b
Singee <imsingee@gmail.com>
2018-09-16 09:12:15 +0800

    server trolley count bug fixed

--- server/controllers/trolley.js
+++ server/controllers/trolley.js
@@ -5,13 +5,15 @@ module.exports = {
         let user = ctx.state.$wxInfo.userinfo.openId
         let product_id = ctx.request.body.product.id
 
+        ctx.state.data = {user, product_id}
+
         let obj = await DB.query('SELECT * FROM trolley_user WHERE trolley_user.id = ? AND trolley_user.user = ?', [product_id, user])
 
         if (obj.length > 0) {
-            let count = obj[0].count
-            DB.query('UPDATE trolley_user SET count = ? WHERE trolley_user.id = ? AND trolley_user.user = ?', [count, product_id, user])
+            let count = obj[0].count + 1
+            await DB.query('UPDATE trolley_user SET count = ? WHERE trolley_user.id = ? AND trolley_user.user = ?', [count, product_id, user])
         } else {
-            DB.query('INSERT INTO trolley_user(id, count, user) VALUES (?, ?, ?)', [product_id, 1, user])
+            await DB.query('INSERT INTO trolley_user(id, count, user) VALUES (?, ?, ?)', [product_id, 1, user])
         }
     }
 }
