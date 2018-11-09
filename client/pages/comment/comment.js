822925dc7b7e5acb4a3cfa02ef29b18f1ab26fc0
Singee <imsingee@gmail.com>
2018-11-09 13:09:55 +0800

    comment page wxml+wxss

--- client/pages/comment/comment.js
+++ client/pages/comment/comment.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    id: -1,
+    name: '',
+    price: -1,
+    image: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
+    this.setData({
+      id: options.id,
+      name: options.name,
+      price: options.price,
+      image: options.image
+    })
   },
 
   /**
